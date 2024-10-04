d9b7629ecdb377487665d1b4e4779aa86f34a33b
Lucas Schmidt <lucas.schmidt015@gmail.com>
2024-10-03 22:09:33 -0300

    segunda parte do trabalho.

--- trabalho/CasaSegura.vhd
+++ trabalho/CasaSegura.vhd
@@ -25,16 +25,54 @@ entity casa_segura_alarme is
 end entity casa_segura_alarme;
 
 architecture behav_FLUXO of casa_segura_alarme is
-    process(sw)
-    begin 
-        if (sw(2) == '1') then 
-            if (sw(0) == '1' OR sw(1) == '1') then 
-                ledr <= "000000001";
+    begin
+        process(sw)
+        begin 
+            if (sw(2) = '1') then 
+                if (sw(0) = '1' OR sw(1) = '1') then 
+                    ledr <= "0000000001";
+                else
+                    ledr <= "0000000000";
+                end if;
             else
-                ledr <= "000000000";
-            end if;
-        else
-            ledr <= "000000000";
-        end if;        
-    end process
+                ledr <= "0000000000";
+            end if;        
+        end process;
 end behav_FLUXO;
+
+
+---------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity casa_segura_caixa_agua is
+    port (
+        -- 8 - 9 => entrada de A
+        -- 7 => Entrada B
+        --ledr9 - valvula 
+        --ledr8 - bomba
+        sw: in std_logic_vector(9 downto 0);
+        ledr: out std_logic_vector(9 downto 0)
+    );
+end entity casa_segura_caixa_agua;
+
+architecture behav_FLUXO2 of casa_segura_caixa_agua is
+    begin
+        process(sw)
+        begin 
+            if ((sw(9) = '0' and sw(8) = '0') or  (sw(9) = '0' and sw(8) = '1')) then 
+                if (sw(7) = '0') then 
+                    ledr <= "1100000000";    
+                else 
+                    ledr <= "1000000000";
+                end if;
+            else 
+                if (sw(7) = '0') then                     
+                    ledr <= "0100000000";
+                else 
+                    ledr <= "0000000000";                  
+                end if;
+            end if;
+        end process;
+end behav_FLUXO2;
