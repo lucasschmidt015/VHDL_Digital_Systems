940b8c819e39ecc013b464fa1fa69882aa4a22c2
Lucas Schmidt <lucas.schmidt015@gmail.com>
2024-10-03 22:37:00 -0300

    foi

--- trabalho/CasaSegura.vhd
+++ trabalho/CasaSegura.vhd
@@ -48,10 +48,12 @@ use ieee.std_logic_1164.all;
 
 entity casa_segura_caixa_agua is
     port (
-        -- 8 - 9 => entrada de A
+        -- 9 - 8 => entrada de A
         -- 7 => Entrada B
         --ledr9 - valvula 
         --ledr8 - bomba
+
+        
         sw: in std_logic_vector(9 downto 0);
         ledr: out std_logic_vector(9 downto 0)
     );
@@ -76,3 +78,57 @@ architecture behav_FLUXO2 of casa_segura_caixa_agua is
             end if;
         end process;
 end behav_FLUXO2;
+
+
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity principal is
+    port (
+        -- 9 - 8 => entrada de A
+        -- 7 => Entrada B
+        --ledr9 - valvula 
+        --ledr8 - bomba
+
+        -- 0 => porta
+        -- 1 => Janela
+        -- 2 => modo seguro
+        
+        sw: in std_logic_vector(9 downto 0);
+        ledr: out std_logic_vector(9 downto 0)
+    );
+end entity principal;
+
+architecture behav_FLUXO3 of principal is
+    component casa_segura_alarme IS
+    port (        
+        sw1: in std_logic_vector(9 downto 0);
+        ledr1: out std_logic_vector(9 downto 0)
+    );
+    END component;
+
+    component casa_segura_caixa_agua IS
+    port (        
+        sw2: in std_logic_vector(9 downto 0);
+        ledr2: out std_logic_vector(9 downto 0)
+    );
+    END component;
+
+    signal aux1, aux2: std_logic_vector(9 downto 0);
+
+begin
+    port1: casa_segura_alarme
+    port map(
+        sw1 => sw,
+        ledr1 =>  aux1
+    );
+
+    port2: casa_segura_caixa_agua
+    port map(
+        sw2 => sw,
+        ledr2 =>  aux2
+    );
+    ledr <= aux1 or aux2;
+end behav_FLUXO3;
